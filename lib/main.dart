05451e3aacf9e2b5b7e21e1fa6c3e870e410c6c1
imrishuroy <rishukumar.prince@gmail.com>
2022-06-27 03:54:23 +0530

    Refactured

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '/translations/codegen_loader.g.dart';
 import '/repositories/parcel/parcel_repository.dart';
 import '/repositories/location/location_repository.dart';
 import '/repositories/store/store_repository.dart';
